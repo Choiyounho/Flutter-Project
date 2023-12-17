4585cb884a11e0de1cd6f8d000cf90a3c1e80238
Choiyounho <maxosa78@gmail.com>
2023-12-18 03:41:08 +0900

    feat: Widget 샘플 적용

---
+++ state/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+      home: Scaffold(
+          body: TestWidget()
+      )
+  ));
+}
+
+class TestWidget extends StatelessWidget {
+  const TestWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Center(
+        child: Text(
+          'Hello, Flutter',
+          style: TextStyle(color: Colors.black, fontSize: 30),
+        ),
+      ),
+    );
+  }
+}
