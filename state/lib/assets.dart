2c449d428f3890441b48d785049f63dfeb7b11f0
Choiyounho <maxosa78@gmail.com>
2023-12-24 03:59:08 +0900

    feat: 플러터 로컬 사진 적용

---
+++ state/lib/assets.dart
@@ -0,0 +1,24 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text('로컬 데이터 사용'),
+      ),
+      body: const Body(),
+    ),
+  ));
+}
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset('assets/images/ic_star.png'),
+    );
+  }
+}
