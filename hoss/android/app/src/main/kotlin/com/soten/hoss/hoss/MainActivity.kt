0c21a56a6e5e53d4b5e85b701a1f6c29382759d0
Choiyounho <maxosa78@gmail.com>
2024-01-02 17:26:59 +0900

    init hoss project

---
+++ hoss/android/app/src/main/kotlin/com/soten/hoss/hoss/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.soten.hoss.hoss
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ hoss/lib/common/cli_common.dart
@@ -0,0 +1,3 @@
+export 'package:hoss/common/dart/extension/collection_extension.dart';
+export 'package:hoss/common/dart/extension/num_duration_extension.dart';
+export 'package:hoss/common/util/async/cli_async.dart';
