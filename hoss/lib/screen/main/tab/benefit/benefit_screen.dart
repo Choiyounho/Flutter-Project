60fbb494518f108863453f2029b9510939142f61
Choiyounho <maxosa78@gmail.com>
2024-01-04 15:55:14 +0900

    feat: BenefitScreen

--- hoss/lib/screen/main/tab/benefit/benefit_screen.dart
+++ hoss/lib/screen/main/tab/benefit/benefit_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hoss/common/common.dart';
+import 'package:hoss/screen/main/tab/benefit/point_button.dart';
 
 class BenefitScreen extends StatefulWidget {
   const BenefitScreen({super.key});
@@ -10,6 +12,16 @@ class BenefitScreen extends StatefulWidget {
 class _BenefitScreenState extends State<BenefitScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Height(30),
+          "혜택".text.white.bold.size(18).make().pSymmetric(h: 20),
+          PointButton(point: 8000),
+          "혜택 더 받기".text.white.bold.size(16).make().pSymmetric(h: 20),
+        ],
+      ),
+    );
   }
 }
